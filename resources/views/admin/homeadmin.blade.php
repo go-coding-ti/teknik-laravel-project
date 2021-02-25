9d903741768ae2a441aaa11d683d534b6110e6a8
Wahyu <wahyuivan999@ymail.com>
2021-02-25 23:30:23 +0800

    feat(layout): update admin form master data layout

--- resources/views/admin/homeadmin.blade.php
+++ resources/views/admin/homeadmin.blade.php
@@ -2,20 +2,14 @@
 @section('content')
     <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800">Admin Profile</h1>
+        <div style="margin-left: 10px;" class="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-list"></i> Master Data Dosen</h1>
         </div>
-        <hr style="margin-top: 20px" class="sidebar-divider my-0">
-
-        <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
-
           <!-- DataTales Example -->
           <!-- Copy drisini -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+              <h6 class="m-0 font-weight-bold text-primary">List Data Dosen</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
@@ -49,46 +43,6 @@
               </div>
             </div>
           </div>
-          <!-- smpe sini -->
-        
-        <!-- Content Row -->
-        <div class="row">
-        <form method="POST" enctype="multipart/form-data" action="/admin/profile">
-        
-        </form>
-        </div>
-
-        <!-- Content Row -->
-
-        <div class="row">
-          
-        </div>
-
-        <!-- Content Row -->
-        <div class="row">
-
-          <!-- Content Column -->
-          <div class="col-lg-6 mb-4">
-
-            <!-- Color System -->
-            <div class="row">
-              <div class="card mb-4">
-                <div class="card-header">
-                  Default Card Example
-                </div>
-                <div class="card-body">
-                  This card uses Bootstrap's default styling with no utility classes added. Global styles are the only things modifying the look and feel of this default card example.
-                </div>
-              </div>
-          </div>
-
-          </div>
-
-          <div class="col-lg-6 mb-4">
-
-          </div>
-        </div>
-
       </div>
       <!-- /.container-fluid -->
 @endsection
