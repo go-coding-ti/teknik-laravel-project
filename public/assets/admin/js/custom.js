e3901b74589a25a44b8fe0e5f8445a7adacc4fe7
Wahyu <wahyuivan999@ymail.com>
2021-03-27 10:43:55 +0800

    feat(Auth): Add auth admin

---
+++ public/assets/admin/js/custom.js
@@ -0,0 +1,16 @@
+$('.modal-global').click(function(event) {
+    event.preventDefault();
+  
+    var url = $(this).attr('href');
+  
+    $("#modal-global").modal('show');
+  
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'html',
+    })
+    .done(function(response) {
+        $("#modal-global").find('.modal-body').html(response);
+    });
+});
